e07f6c0923dadaa3711c69c1b687cb8ddfb3745a
ghost <jhonsnow751@gmail.com>
2020-06-23 12:05:29 -0700

    added time

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-var cors = require("cors");
+
 const crypto = require("crypto");
+
 
 const app = express();
 const port = process.env.PORT || 9000;
 
-app.use(cors());
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.use("/", require("./routes/routes"));
 
--- routes/routes.js
+++ routes/routes.js
@@ -3,22 +3,51 @@ const router = express.Router();
 const bodyParser = require("body-parser");
 var cors = require("cors");
 
+var cron = require("node-cron");
+
+var array = [1, 2];
+
 router.use(cors());
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
 router.route("/").get((req, res) => {
-    res.json({
-        name: "nunnnn",
-        about: "about"
-    });
+  var sec = new Date().getSeconds();
+  var min = new Date().getMinutes();
+  var hour = new Date().getHours();
+  var day = new Date().getDay();
+
+  const { id, name } = req.body;
+
+  var task = cron.schedule(
+    `*/${sec} * * * * *`,
+    () => {
+      var number = array.length + 1;
+      array.push(number);
+      console.log(`${sec} ${min} ${hour} ${day}`);
+      res.json({ id: id, name: name });
+      stoptask();
+    },
+    {
+      scheduled: false,
+    }
+  );
+
+  function stoptask() {
+    task.destroy();
+    console.log("destroyed task");
+  }
+
+  task.start();
 });
 
 router.route("/cool").get((req, res) => {
-    res.json({
-        name: "nunnnn",
-        about: "about"
-    });
+  res.send(array);
+});
+
+router.route("/test").post((req, res) => {
+  const { seconds, minute } = req.body;
+  res.json({ seconds: seconds, minute: minute });
 });
 
 module.exports = router;
