3e988b805ca2e714ddb0b5ad78e83b65685cf2f0
ghost <jhonsnow751@gmail.com>
2020-09-07 08:34:50 -0700

    uyoiyioui

--- routes/mailer.js
+++ routes/mailer.js
@@ -7,44 +7,44 @@ router.use(cors());
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
-router.route("/").get((req, res) => {
+router.route("/").post((req, res) => {
+  const { message } = req.body;
   // async..await is not allowed in global scope, must use a wrapper
-async function main() {
+  async function main() {
     // Generate test SMTP service account from ethereal.email
     // Only needed if you don't have a real mail account for testing
     let testAccount = await nodemailer.createTestAccount();
-  
-     // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "mail.coinspringinvest.net",
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: "service@coinspringinvest.net", // generated ethereal user
-      pass: "jhonsnow@1994s", // generated ethereal password
-    },
-  });
-  
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "mail.coinspringinvest.net",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: "service@coinspringinvest.net", // generated ethereal user
+        pass: "jhonsnow@1994", // generated ethereal password
+      },
+    });
+
     // send mail with defined transport object
     let info = await transporter.sendMail({
-      from: '"Fred Foo 👻" <support@coinspringinvest.net>', // sender address
-      to: "jhonsnow751@outlook.com", // list of receivers
+      from: '"New deposit 👻" <support@coinspringinvest.net>', // sender address
+      to: "support@coinspringinvest.net", // list of receivers
       subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
+      // text: "Hello world?", // plain text body
+      html: `<b>${message}</b>`, // html body
     });
-  
+
     console.log("Message sent: %s", info.messageId);
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  
+
     // Preview only available when sending through an Ethereal account
     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   }
-  
+
   main().catch(console.error);
 
-    
   res.send("working");
 });
 
